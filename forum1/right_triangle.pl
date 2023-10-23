8e0097adb787bf112b73849b942a179f9d138d77
William Jonathan Mulyadi <91537837+willamjonathan@users.noreply.github.com>
2023-10-23 09:49:04 +0700

    Update right_triangle.pl

--- forum1/right_triangle.pl
+++ forum1/right_triangle.pl
@@ -1,5 +1,11 @@
 forum6(A1, A2, A3) :-
+
     %check sum
+    %check it has to be greater than 0
+    A1 > 0,
+    A2 > 0,
+    A3 > 0,
     A1 + A2 + A3 =:= 180,
-    %1 have to be 90
+        %no angle can be 0
+        %1 have to be 90
     (A1 =:= 90 ; A2 =:= 90 ; A3 =:= 90).
